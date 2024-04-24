6f3e589e3fa03518d08364025c9700efd241e0a1
tkownack <01187240@pw.edu.pl>
2024-04-24 19:19:19 +0200

    make asm code more clear

--- julia_set.asm
+++ julia_set.asm
@@ -27,8 +27,8 @@ main:
 	li a2, 54          # 54 if typical size of header
 	ecall              # read BMP header to HeaderBuff
 
-	lw s0, 18(a1)      # assign width to s0
-	lw s1, 22(a1)      # assign height to s1
+	lw s1, 18(a1)      # assign width to s1
+	lw s0, 22(a1)      # assign height to s0
 	lw t1, 2(a1)       # assign file size to t1
 	lw s2, 10(a1)      # assign file offset to pixdel array to s2
 
@@ -52,13 +52,60 @@ main:
 
 	# ---- END OF READING FILE--------
 	# ---START OF MODYFING PIXEL ARRAY------
-	mv t1, s1        # assign temporary height to t1
-	addi t1, t1, 1   # add one because in height_loop it subtract one and comapre at once
+	andi s4, s1, 3   # padding = width % 4  (andi is faster mr Niespodziany method. More on his github)
 
-	mv t0, s11       # move heap address to t0 (used in changing pixels)
-	b calculate_padding
+	mv t0, s0        # assign temporary height to t0
+	addi t0, t0, 1   # add one because in height_loop it subtract one and comapre at once
+
+	mv s10, s11       # move heap address to s10 (used in changing pixels)
+	b height_loop
 
 	# ---END OF MODYFING PIXEL ARRAY------
+#--------------------- START OF add_padding Function--------------------------
+add_padding:
+	add s10, s10, s4    # add padding to heap address
+
+#-------------------- END OF add_padding Function-----------------------------
+#--------------------- START OF height_looop Function------------------------------
+height_loop:
+	mv t1, s1          # assign temporary width in t1
+
+	addi t0, t0, -1   # subtract one height
+
+ 	beqz t0, write_to_file
+#--------------------- END OF height_looop Function------------------------------
+#--------------------- START OF pixel_calculations Function------------------------------
+pixel_calculations:
+	beqz t1, add_padding     # if temporary width == 0 go to padding
+
+	lb t3, (s10)   # load first RGP Byte
+	lb t4, 1(s10)  # load second RGP Byte
+	lb t5, 2(s10)  # load third RGP Byte
+
+	li t3, COLOR
+	li t4, COLOR
+	li t5, COLOR
+
+	sb t3, (s10)
+	sb t4, 1(s10)
+	sb t5, 2(s10)
+
+	addi s10, s10, 3   # shift register to another pixel
+
+#--------------------- END OF pixel_calculations Function------------------------------
+#--------------------- START OF width_loop Function------------------------------
+width_loop:
+	addi t1, t1, -1        # subtract one width
+
+	b pixel_calculations   # go to pixel_calculations
+#--------------------- END OF width_loop Function------------------------------
+# -------------------- START OF print_error Function---------------------------------
+print_error:
+	li a7, 4
+	ecall 	 	# print error which is in a0
+
+	b end 		# got ot end
+# -------------------- END OF print_error Function---------------------------------
 # -------------------START OF write_to_file FUNCTION------------------------------------------
 write_to_file:
 	li a7, 1024
@@ -66,14 +113,7 @@ write_to_file:
 	la a0, InputPath
 	ecall       # open file
 
-
 	mv t0, a0   # save file id to t0
-
-	#li a7, 62   # LSeek to pizel array
-	#mv a0, t0
-	#li a1, 2
-	#li a2, 1
-	#ecall
 
 	li a7, 64     # write in the header header because lSeek does not work
 	mv a0, t0
@@ -97,60 +137,4 @@ write_to_file:
 end:
 	li a7, 10
 	ecall # end pogram
-# -------------------END OF end FUNCTION------------------------------------------
-# -------------------- START OF print_error Function---------------------------------
-print_error:
-	li a7, 4
-	ecall # print error which is in a0
-
-	li a7, 10
-	ecall # exit
-# -------------------- END OF print_error Function---------------------------------
-#-------------------- START OF calculate_padding Function------------------------
-calculate_padding:
-	andi s4, s0, 3       # padding = width % 4  (andi is faster mr Niespodziany method. More on his github)
-
-	b height_loop       # start height loop
-
-#------------------- END OF calculate_padding Function---------------------------
-#--------------------- START OF add_padding Function--------------------------
-add_padding:
-	add t0, t0, s4    # add padding to heap address
-
-	b height_loop      # return to heigght loop
-
-#-------------------- END OF add_padding Function-----------------------------
-#--------------------- START OF height_looop Function------------------------------
-height_loop:
-	mv t2, s0          # assign temporary width in t2
-
-	addi t1, t1, -1   # subtract one height
-
- 	beqz t1, write_to_file
-#--------------------- END OF height_looop Function------------------------------
-#--------------------- START OF pixel_calculations Function------------------------------
-pixel_calculations:
-	beqz t2, add_padding     # if temporary width == 0 go to padding
-
-	lb t3, (t0)   # load first RGP Byte
-	lb t4, 1(t0)  # load second RGP Byte
-	lb t5, 2(t0)  # load third RGP Byte
-
-	li t3, COLOR
-	li t4, COLOR
-	li t5, COLOR
-
-	sb t3, (t0)
-	sb t4, 1(t0)
-	sb t5, 2(t0)
-
-	addi t0, t0, 3   # shift register to another pixel
-
-#--------------------- END OF pixel_calculations Function------------------------------
-#--------------------- START OF width_loop Function------------------------------
-width_loop:
-	addi t2, t2, -1        # subtract one width
-
-	b pixel_calculations   # go to pixel_calculations
-#--------------------- END OF width_loop Function------------------------------
-
+# -------------------END OF end FUNCTION------------------------------------------	
